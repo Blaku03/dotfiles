19abafc0dbf02e090fdd63a482d071cf21aafc69
Blaku03 <the.bainow03@gmail.com>
2025-04-05 22:17:23 +0200

    Run zellij on start and fix the venv for starship

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,13 @@ starship init fish | source
 
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
+
+# Automatically attach to the Zellij session named "Blaku"
+if status is-interactive
+    # Check if already inside a Zellij session
+    if not set -q ZELLIJ
+        set ZELLIJ_AUTO_EXIT true  # Exit shell cleanly when Zellij exits
+        set ZELLIJ_AUTO_ATTACH true  # Automatically attach to an existing session if available
+        zellij attach Blaku || zellij --session Blaku  # Attach to "Blaku" or create it if it doesn't exist
+    end
+end
